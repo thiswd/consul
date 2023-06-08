dc196add05c05610893aea05e660d13920369835
Thiago Sawada <tms1_tms2@hotmail.com>
2023-06-07 21:49:11 -0300

    feat: associations for Review model

--- app/models/legislation/review.rb
+++ app/models/legislation/review.rb
@@ -2,6 +2,27 @@ class Legislation::Review < ApplicationRecord
   acts_as_paranoid column: :hidden_at
   include ActsAsParanoidAliases
 
-  belongs_to :author, -> { with_hidden }, class_name: "User", inverse_of: :legislation_reviews
-  belongs_to :process, foreign_key: "legislation_process_id", inverse_of: :reviews
+  belongs_to :process,
+    foreign_key: "legislation_process_id",
+    inverse_of: :reviews
+
+  belongs_to :author, -> { with_hidden },
+    class_name: "User",
+    inverse_of: :legislation_reviews
+
+  has_many :section_types,
+    foreign_key: "legislation_review_id",
+    class_name: "Legislation::Review::SectionType",
+    inverse_of: :review,
+    dependent: :destroy
+
+  has_many :sections,
+    foreign_key: "legislation_review_id",
+    class_name: "Legislation::Review::Section",
+    inverse_of: :review,
+    dependent: :destroy
+
+  def root_sections
+    sections.roots.order(:id)
+  end
 end
